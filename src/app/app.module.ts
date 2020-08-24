1863c7301403cca047e6019822c95da31179a6e6
JDCASTRO-IENM <57963583+JDCASTRO-IENM@users.noreply.github.com>
2020-08-23 23:10:13 -0500

    First commit. integración Angular material y creación componente toolbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatModule } from './mat/mat.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/statics/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ToolbarComponent],
+  imports: [BrowserModule, AppRoutingModule, MatModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
