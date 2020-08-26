8938fb2ef6ab5b2b9347a2224e8e6c6958f96768
JDCASTRO-IENM <57963583+JDCASTRO-IENM@users.noreply.github.com>
2020-08-25 22:35:39 -0500

    Cambio ext css a scss

--- src/app/modules/predio/predio.component.ts
+++ src/app/modules/predio/predio.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-predio',
   templateUrl: './predio.component.html',
-  styleUrls: ['./predio.component.css']
+  styleUrls: ['./predio.component.scss'],
 })
 export class PredioComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   @Output() toggleSideBarForMe: EventEmitter<any> = new EventEmitter();
